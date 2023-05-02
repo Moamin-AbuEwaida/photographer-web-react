e3a899ea08c9b728cf66fddb6e738693beff11fb
Moamin <moamin.salamah@gmail.com>
2023-05-02 10:42:35 +0200

    portfolio page motion

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,17 +4,31 @@ import Image1 from "../img/portfolio/1.png";
 import Image2 from "../img/portfolio/2.png";
 import Image3 from "../img/portfolio/3.png";
 import Image4 from "../img/portfolio/4.png";
-// import Image5 from '../img/portfolio/5.png'
-// import Image6 from '../img/portfolio/6.png'
+
 import { Link } from "react-router-dom";
+
+import { motion } from "framer-motion";
+import { transition1 } from "../transition";
 
 const Portfolio = () => {
   return (
-    <section className="section">
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={{ transition1 }}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8">
           {/* text */}
-          <div className="flex flex-col lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "-80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-80%" }}
+            transition={{ transition1 }}
+            className="flex flex-col lg:items-start"
+          >
             <h1 className="h1">Portfolio</h1>
             <p className="mb-12 max-w-sm">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
@@ -30,7 +44,7 @@ const Portfolio = () => {
             <Link to={"/contact"} className="btn mb-[30px] mx-auto lg:mx-0">
               Hire me
             </Link>
-          </div>
+          </motion.div>
           {/* images */}
           <div className="grid grid-cols-2 gap-2">
             <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
@@ -64,7 +78,7 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
